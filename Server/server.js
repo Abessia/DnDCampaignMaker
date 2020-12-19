bb793d53610684c555dd2d22db76aded1520430d
Abessia <rebecca.e.wiegel@gmail.com>
2020-12-19 16:34:41 -0700

    Implemented basic server and front end

---
+++ Server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+module.exports = app;
