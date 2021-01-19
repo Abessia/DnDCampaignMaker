f1ac38831c3314220580c1aa2942475baff548d6
Abessia <rebecca.e.wiegel@gmail.com>
2021-01-19 09:07:29 -0700

    Deleted a few unused files; added basic structure for Chrome extension

---
+++ ChromeExtension/dmg-script.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+
+/*
+This content script places a button on the table of contents page for the DMG on dndbeyond's website
+*/
+
+const pageHeading = document.getElementsByClassName('page-heading__content');
+console.log(pageHeading);
